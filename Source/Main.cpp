2d09b252f53606ffa9071a7c4a021023e3453eed
Adria Sellares <adsesa93@gmail.com>
2022-09-19 19:31:45 +0200

    Finish Adding ImGUI to the project
    Co-Authored-By: Yogui <zhidbcn@gmail.com>

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,10 @@
+#include "imgui.h"
 #include <stdlib.h>
 #include "Globals.h"
 #include "Application.h"
 #include "MemLeaks.h"
 
-#include "External/SDL/include/SDL.h"
-#include "imgui.h"
+#include "SDL.h"
 
 enum main_states
 {
@@ -15,82 +15,84 @@ enum main_states
 	MAIN_EXIT
 };
 
-//int main(int argc, char** argv)
-//{
-//	srand((unsigned)time(0));
-//	LOG("Starting game '%s'...", TITLE);
-//
-//	int main_return = EXIT_FAILURE;
-//	main_states state = MAIN_CREATION;
-//	Application* App = nullptr;
-//
-//	//ImGui::CreateContext();
-//
-//	while (state != MAIN_EXIT)
-//	{
-//		switch (state)
-//		{
-//		case MAIN_CREATION:
-//
-//			LOG("-------------- Application Creation --------------");
-//			App = new Application();
-//			state = MAIN_START;
-//			break;
-//
-//		case MAIN_START:
-//
-//			LOG("-------------- Application Init --------------");
-//			if (App->Init() == false)
-//			{
-//				LOG("Application Init exits with ERROR");
-//				state = MAIN_EXIT;
-//			}
-//			else
-//			{
-//				state = MAIN_UPDATE;
-//				LOG("-------------- Application Update --------------");
-//			}
-//
-//			break;
-//
-//		case MAIN_UPDATE:
-//		{
-//			int update_return = App->Update();
-//
-//			if (update_return == UPDATE_ERROR)
-//			{
-//				LOG("Application Update exits with ERROR");
-//				state = MAIN_EXIT;
-//			}
-//
-//			if (update_return == UPDATE_STOP)
-//				state = MAIN_FINISH;
-//		}
-//		break;
-//
-//		case MAIN_FINISH:
-//
-//			LOG("-------------- Application CleanUp --------------");
-//			if (App->CleanUp() == false)
-//			{
-//				LOG("Application CleanUp exits with ERROR");
-//			}
-//			else
-//				main_return = EXIT_SUCCESS;
-//
-//			state = MAIN_EXIT;
-//
-//			break;
-//
-//		}
-//	}
-//
-//	delete App;
-//	App = nullptr;
-//
-//	ReportMemoryLeaks();
-//
-//	LOG("\nBye :)\n");
-//
-//	return main_return;
-//}
+int main(int argc, char** argv)
+{
+	srand((unsigned)time(0));
+	LOG("Starting game '%s'...", TITLE);
+
+	int main_return = EXIT_FAILURE;
+	main_states state = MAIN_CREATION;
+	Application* App = nullptr;
+
+	ImGui::CreateContext();
+
+	while (state != MAIN_EXIT)
+	{
+		switch (state)
+		{
+		case MAIN_CREATION:
+
+			LOG("-------------- Application Creation --------------");
+			App = new Application();
+			state = MAIN_START;
+			break;
+
+		case MAIN_START:
+
+			LOG("-------------- Application Init --------------");
+			if (App->Init() == false)
+			{
+				LOG("Application Init exits with ERROR");
+				state = MAIN_EXIT;
+			}
+			else
+			{
+				state = MAIN_UPDATE;
+				LOG("-------------- Application Update --------------");
+			}
+
+			break;
+
+		case MAIN_UPDATE:
+		{
+			int update_return = App->Update();
+
+			if (update_return == UPDATE_ERROR)
+			{
+				LOG("Application Update exits with ERROR");
+				state = MAIN_EXIT;
+			}
+
+			if (update_return == UPDATE_STOP)
+				state = MAIN_FINISH;
+		}
+		break;
+
+		case MAIN_FINISH:
+
+			LOG("-------------- Application CleanUp --------------");
+			if (App->CleanUp() == false)
+			{
+				LOG("Application CleanUp exits with ERROR");
+			}
+			else
+				main_return = EXIT_SUCCESS;
+
+			state = MAIN_EXIT;
+
+			break;
+
+		}
+	}
+
+	delete App;
+	App = nullptr;
+
+	ImGui::DestroyContext();
+
+	ReportMemoryLeaks();
+
+	LOG("\nBye :)\n");
+
+	return main_return;
+}
--- Source/Primitive.cpp
+++ Source/Primitive.cpp
@@ -169,7 +169,7 @@ Sphere::Sphere(float radius) : Primitive(), radius(radius)
 
 void Sphere::InnerRender() const
 {
-	glutSolidSphere(radius, 25, 25);
+	//glutSolidSphere(radius, 25, 25);
 }
 
 // CYLINDER ============================================
