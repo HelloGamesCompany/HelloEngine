5f20d7ea5b8d3e2c2db26be0f67b6ffbba75dc7a
Adria Sellares <adsesa93@gmail.com>
2022-09-25 14:44:54 +0200

    Make main windoe resizable

--- Source/Globals.h
+++ Source/Globals.h
@@ -63,7 +63,7 @@ enum class UpdateStatus
 #define SCREEN_HEIGHT 720
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
-#define WIN_RESIZABLE false
+#define WIN_RESIZABLE true
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
