6e91e8a6a3c108d71b49e3d9e3bb587c4cfc442a
Adria Sellares <adsesa93@gmail.com>
2022-09-29 21:45:57 +0200

    Add Sphere to Mesh class (there are some issues to be solved yet)

--- Source/Primitive.h
+++ Source/Primitive.h
@@ -22,6 +22,7 @@ public:
 	~Mesh();
 
 	void InitAsCube(float3 position, float3 transform);
+	void InitAsSphere(float3 position, float3 transform);
 
 	void Update();
 
@@ -36,99 +37,99 @@ private:
 	std::vector<float3>* _originalShape;
 };
 
-namespace Primitives
-{
-	class Primitive
-	{
-	public:
-		Primitive();
-
-		virtual void	Render() const;
-		virtual void	InnerRender();
-		void			SetPos(float x, float y, float z);
-		void			SetRotation(float angle, const vec3& u);
-		void			Scale(float x, float y, float z);
-		PrimitiveTypes	GetType() const;
-
-	public:
-
-		Color color;
-		mat4x4 transform;
-		bool axis = true, wire = true;
-
-	protected:
-		PrimitiveTypes type;
-	};
-
-	// ============================================
-	//TODO: Make primitives dynamic, so their size and position may change.
-	class Cube : public Primitive
-	{
-	public:
-		Cube();
-		Cube(float sizeX, float sizeY, float sizeZ);
-		~Cube();
-		
-		void InnerRender() override;
-
-	public:
-		vec3 size;
-		std::vector<float3>* vertices;
-		std::vector<uint>* indices;
-	private:
-		void GenerateVertexBuffer();
-		uint VBO = 0;
-		uint IBO = 0;
-		uint VAO = 0;
-	};
-
-	// ============================================
-	class Sphere : public Primitive
-	{
-	public:
-		Sphere();
-		Sphere(float radius);
-		void InnerRender() override;
-	public:
-		float radius = 0;
-	};
-
-	// ============================================
-	class Cylinder : public Primitive
-	{
-	public:
-		Cylinder();
-		Cylinder(float radius, float height);
-		void InnerRender() override;
-	public:
-		float radius = 0;
-		float height = 0;
-	};
-
-	// ============================================
-	class Line : public Primitive
-	{
-	public:
-		Line();
-		Line(float x, float y, float z);
-		void InnerRender() override;
-	public:
-		vec3 origin;
-		vec3 destination;
-	};
-
-	// ============================================
-	class Plane : public Primitive
-	{
-	public:
-		Plane();
-		Plane(float x, float y, float z, float d);
-		void InnerRender() override;
-	public:
-		vec3 normal;
-		float constant = 0;
-	};
-
-}
+//namespace Primitives
+//{
+//	class Primitive
+//	{
+//	public:
+//		Primitive();
+//
+//		virtual void	Render() const;
+//		virtual void	InnerRender();
+//		void			SetPos(float x, float y, float z);
+//		void			SetRotation(float angle, const vec3& u);
+//		void			Scale(float x, float y, float z);
+//		PrimitiveTypes	GetType() const;
+//
+//	public:
+//
+//		Color color;
+//		mat4x4 transform;
+//		bool axis = true, wire = true;
+//
+//	protected:
+//		PrimitiveTypes type;
+//	};
+//
+//	// ============================================
+//	//TODO: Make primitives dynamic, so their size and position may change.
+//	class Cube : public Primitive
+//	{
+//	public:
+//		Cube();
+//		Cube(float sizeX, float sizeY, float sizeZ);
+//		~Cube();
+//		
+//		void InnerRender() override;
+//
+//	public:
+//		vec3 size;
+//		std::vector<float3>* vertices;
+//		std::vector<uint>* indices;
+//	private:
+//		void GenerateVertexBuffer();
+//		uint VBO = 0;
+//		uint IBO = 0;
+//		uint VAO = 0;
+//	};
+//
+//	// ============================================
+//	class Sphere : public Primitive
+//	{
+//	public:
+//		Sphere();
+//		Sphere(float radius);
+//		void InnerRender() override;
+//	public:
+//		float radius = 0;
+//	};
+//
+//	// ============================================
+//	class Cylinder : public Primitive
+//	{
+//	public:
+//		Cylinder();
+//		Cylinder(float radius, float height);
+//		void InnerRender() override;
+//	public:
+//		float radius = 0;
+//		float height = 0;
+//	};
+//
+//	// ============================================
+//	class Line : public Primitive
+//	{
+//	public:
+//		Line();
+//		Line(float x, float y, float z);
+//		void InnerRender() override;
+//	public:
+//		vec3 origin;
+//		vec3 destination;
+//	};
+//
+//	// ============================================
+//	class Plane : public Primitive
+//	{
+//	public:
+//		Plane();
+//		Plane(float x, float y, float z, float d);
+//		void InnerRender() override;
+//	public:
+//		vec3 normal;
+//		float constant = 0;
+//	};
+//
+//}
 
 #endif // !__PRIMITIVE_H__
--- Source/RenderManager.cpp
+++ Source/RenderManager.cpp
@@ -5,12 +5,16 @@ RenderManager::RenderManager()
 {
     CreateBuffers();
 
-    for (int i = 0; i < 100; i++)
+   /* for (int i = 0; i < 100; i++)
     {
         Mesh cube;
         cube.InitAsCube({ 0.0f + i,0.0f,0.0f }, { 1.0f,1.0f,1.0f });
         meshes.push_back(cube);
-    }
+    }*/
+    Mesh sphere;
+    sphere.InitAsSphere({ 1.0f, 1.0f, 1.0f }, { 4.0f,4.0f,4.0f });
+
+    meshes.push_back(sphere);
 }
 
 RenderManager::~RenderManager()
