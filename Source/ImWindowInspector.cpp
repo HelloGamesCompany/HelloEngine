a5344016e55050cc4c64fc3c6f17f65d733da566
Yogui <972081300@qq.com>
2022-10-17 22:21:51 +0200

    Testing ImGui Func for undo/redo

--- Source/ImWindowInspector.cpp
+++ Source/ImWindowInspector.cpp
@@ -37,19 +37,27 @@ void ImWindowInspector::Update()
 						float3 tempPos = selectGameobject->GetComponent<TransformComponent>()->localTransform.position;
 						float3 tempRot = selectGameobject->GetComponent<TransformComponent>()->localTransform.rotation;
 						float3 tempScale = selectGameobject->GetComponent<TransformComponent>()->localTransform.scale;
-
-						if (ImGui::DragFloat3("position", &tempPos[0], 0.1f))
+						
 						{
+							ImGui::DragFloat3("position", &tempPos[0], 0.1f);
+
+							if (ImGui::IsItemActivated())
+							{
+								static float positionX = 0;
+								positionX = tempPos.x;
+								std::cout << "A= " << positionX << std::endl;
+							}
 							selectGameobject->GetComponent<TransformComponent>()->SetPosition(tempPos);
 						}
-
-						if (ImGui::DragFloat3("rotation", &tempRot[0], 0.1f))
+						
+						
 						{
+							ImGui::DragFloat3("rotation", &tempRot[0], 0.1f);
 							selectGameobject->GetComponent<TransformComponent>()->SetRotation(tempRot);
 						}
 
-						if (ImGui::DragFloat3("scale", &tempScale[0], 0.1f))
 						{
+							ImGui::DragFloat3("scale", &tempScale[0], 0.1f);
 							selectGameobject->GetComponent<TransformComponent>()->SetScale(tempScale);
 						}
 					}
