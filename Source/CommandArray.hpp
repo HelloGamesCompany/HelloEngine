b4916eb5fb612fe0792f174f8c81d9d7f8abd6cd
Yogui <zhidbcn@gmail.com>
2022-10-21 10:40:29 +0200

    Fixed CommandArray relese bug
    - Fixed CommandArray release bug
    - Add comment into CommandArray

--- Source/CommandArray.hpp
+++ Source/CommandArray.hpp
@@ -4,33 +4,60 @@ namespace Htool
 {
 	template<class T>
 
+	/// <summary>
+	/// Command use array
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	class CommandArray
 	{
 	public: 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="size">: max undo action should be size - 1 </param>
+		/// <param name="isPointer">: if is pointer, any default value will be nullptr</param>
 		CommandArray(const int size, const bool isPointer);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="size">: max undo action should be size - 1 </param>
+		/// <param name="value">: default value for init array, just use with no pointer variable </param>
+		/// <param name="isPointer">: in the pointer case, any default value will be nullptr, ignore the value</param>
 		CommandArray(const int size,T value, const bool isPointer);
 
 		~CommandArray();
 
 		void push(T value);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="successful">: use for check if undo action is realize successful</param>
+		/// <returns> return the undo element, do not use if is not seccessful </returns>
 		T undo(bool& successful);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="successful">: use for check if redo action is realize successful</param>
+		/// <returns> return the undo element, do not use if is not seccessful </returns>
 		T redo(bool& successful);
 
+		/// <summary>
+		/// Get data with index, use with caution!!!
+		/// </summary>
+		/// <returns></returns>
 		T& get(const int index);
 
 		int size() { return _size; }
 
 	private:
-
 		int next(int index);
 
 		int previous(int index);
 
 	private:
-
 		T* _arr = nullptr;
 
 		bool _isPointer = false;
@@ -48,6 +75,14 @@ namespace Htool
 		_capacity = size;
 
 		_begin = _end = _current = 0;
+
+		if (_isPointer)
+		{
+			for (int i = 0; i < _capacity; i++)
+			{
+				_arr[i] = nullptr;
+			}
+		}
 	}
 
 	template<class T>
@@ -61,21 +96,32 @@ namespace Htool
 
 		_begin = _end = _current = 0;
 
-		for (size_t i = 0; i < _capacity; i++)
+		if (_isPointer)
 		{
-			_arr[i] = value;
+			for (int i = 0; i < _capacity; i++)
+			{
+				_arr[i] = nullptr;
+			}
+		}
+		else
+		{
+			for (int i = 0; i < _capacity; i++)
+			{
+				_arr[i] = value;
+			}
 		}
 	}
 
 	template<class T>
 	inline CommandArray<T>::~CommandArray()
 	{
-		//TODO release begin to end
 		if(_isPointer)
 		{
-			for (size_t i = 0; i < _capacity; i++)
+			for (int i = 0; i < _capacity; i++)
 			{
-				RELEASE(_arr[i]);
+				delete _arr[i];
+
+				_arr[i] = nullptr;
 			}
 		}
 
@@ -85,7 +131,7 @@ namespace Htool
 	template<class T>
 	inline void CommandArray<T>::push(T value)
 	{
-		if(!_isPointer)
+		if(!_isPointer) // No pointer case
 		{
 			_arr[_current] = value;
 
@@ -95,27 +141,35 @@ namespace Htool
 
 			_size = _size >= _capacity ? _capacity : (_size + 1);
 		}
-		else
+		else // pointer case
 		{
-			if (_current != _end)
+			// free obsolete datas memory
+			if (_current != _end) 
 			{
 				for (int i = _current; i != _end ; i = next(i))
 				{
-					RELEASE(_arr[i]);
+					delete _arr[i];
+
+					_arr[i] = nullptr;
 				}
 			}
 
+			// add new command
 			_arr[_current] = value;
 
 			_end = _current = next(_current);
 
+			// check if the array is full, in the case, we free _begin position data and move this to the next position
 			if(_current == _begin)
 			{
-				RELEASE(_arr[_begin]);
+				delete _arr[_begin];
+
+				_arr[_begin] = nullptr;
 
 				_begin = next(_begin);
 			}
 
+			// check & update size
 			_size = _size >= _capacity ? _capacity : (_size + 1);
 		}
 	}
@@ -123,6 +177,7 @@ namespace Htool
 	template<class T>
 	inline T CommandArray<T>::undo(bool& successful)
 	{
+		// check if we can undo more step or not
 		if (_current == _begin) 
 		{
 			successful = false;
@@ -132,9 +187,9 @@ namespace Htool
 
 		successful = true;
 
-		_size = _size <= 0 ? 0 : (_size - 1);
-
 		_current = previous(_current);
+
+		_size = _size <= 0 ? 0 : (_size - 1);
 
 		return _arr[_current];
 	}
@@ -142,11 +197,12 @@ namespace Htool
 	template<class T>
 	inline T CommandArray<T>::redo(bool& successful)
 	{
+		// check if we can redo more step or not
 		if (_current == _end) 
 		{
 			successful = false;
 
-			return _arr[_current-1];
+			return _arr[previous(_current)];
 		}
 
 		successful = true;
@@ -155,7 +211,7 @@ namespace Htool
 
 		_current = next(_current);
 
-		return _arr[_current-1];
+		return _arr[previous(_current)];
 	}
 
 	template<class T>
