79d8b72e300fe05112de3bc36c1fc28f27a71f9f
Adria Sellares <adsesa93@gmail.com>
2022-12-01 12:26:51 +0100

    Update ImWindowResources

--- Source/ImWindowResources.cpp
+++ Source/ImWindowResources.cpp
@@ -15,19 +15,27 @@ void ImWindowResources::Update()
 {
 	if (ImGui::Begin(windowName.c_str()))
 	{
-		std::map<std::string, uint> alphabeticallyOrderedResources;
-
-		for (auto& resource : ModuleResourceManager::resources)
+		ImGui::TextWrapped("Models are not Resources, only Meshes. Mesh Resources get created when loaded, instead of when imported, because we can only import Models, not Meshes.");
+		if (ImGui::CollapsingHeader("Meshes"))
 		{
-			alphabeticallyOrderedResources[resource.second->debugName] = resource.first;
-		}
-
-		for (auto& resource : alphabeticallyOrderedResources)
-		{
-			if (ModuleResourceManager::resources[resource.second]->type != ResourceType::MODEL) // Ignore MODEL resources, because their reference count is not necessary to track, only meshes's.
+			for (auto& resource : ModuleResourceManager::resources)
 			{
-				ImGui::Text(ModuleResourceManager::resources[resource.second]->debugName.c_str()); ImGui::SameLine();
-				ImGui::TextColored(ImVec4(1, 0, 1, 1), std::to_string(ModuleResourceManager::resources[resource.second]->referenceCount).c_str());
+				if (resource.second->type == ResourceType::MESH)
+				{
+					ImGui::Text(resource.second->debugName.c_str()); ImGui::SameLine();
+					ImGui::TextColored(ImVec4(1, 0, 1, 1), std::to_string(resource.second->referenceCount).c_str());
+				}
+			}
+		}
+		if (ImGui::CollapsingHeader("Textures"))
+		{
+			for (auto& resource : ModuleResourceManager::resources)
+			{
+				if (resource.second->type == ResourceType::TEXTURE) // Ignore MODEL resources, because their reference count is not necessary to track, only meshes's.
+				{
+					ImGui::Text(resource.second->debugName.c_str()); ImGui::SameLine();
+					ImGui::TextColored(ImVec4(1, 0, 1, 1), std::to_string(resource.second->referenceCount).c_str());
+				}
 			}
 		}
 	}
