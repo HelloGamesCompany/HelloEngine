0a25bd61c65fdaf9bff488297b6366fe016d24e4
Yogui <972081300@qq.com>
2022-12-28 17:28:30 +0100

    merge

--- Output/Assets/Scripts/Creationtest.h
+++ Output/Assets/Scripts/Creationtest.h
@@ -7,14 +7,10 @@ class Creationtest : HelloBehavior
 public:
 void Start() override; 
 void Update() override;
-
-float creationTestFloat = 11.0f;
 };
 HELLO_ENGINE_API_C Creationtest* CreateCreationtest(ScriptToInspectorInterface* script)
 {
-	Creationtest* classInstance = new Creationtest();
+Creationtest* classInstance = new Creationtest();
 
-	script->AddDragFloat("creation test 2", &classInstance->creationTestFloat);
-
-	return classInstance;
+return classInstance;
 }
