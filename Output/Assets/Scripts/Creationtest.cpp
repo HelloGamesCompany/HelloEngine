e080abd5312c3a372cb831b6e5b551f2d548db29
Adria Sellares <adsesa93@gmail.com>
2022-12-28 18:43:30 +0100

    Fix Debug differences between DLL and EXE projects

--- Output/Assets/Scripts/Creationtest.cpp
+++ Output/Assets/Scripts/Creationtest.cpp
@@ -1,9 +1,10 @@
 #include "Creationtest.h"
+#include "CSL.h"
 void Creationtest::Start()
 {
 
 }
 void Creationtest::Update()
 {
-
+	API::CSL::Log("Creationtest update 2");
 }
--- Output/Assets/Scripts/Creationtest.h
+++ Output/Assets/Scripts/Creationtest.h
@@ -7,12 +7,14 @@ class Creationtest : HelloBehavior
 public:
 void Start() override;
 void Update() override;
+public:
+	float creationTestFloat = 1.0f;
 };
 HELLO_ENGINE_API_C Creationtest* CreateCreationtest(ScriptToInspectorInterface* script)
 {
 Creationtest* classInstance = new Creationtest();
 
-	//script->AddDragFloat("creation test 2", &classInstance->creationTestFloat);
+	script->AddDragFloat("creation test", &classInstance->creationTestFloat);
 
 	return classInstance;
 }
