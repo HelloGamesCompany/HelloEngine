a5a210793b834ddd32b216e1d33433088502d4ba
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2023-02-27 19:30:41 +0100

    Component UIButton Started

--- Source/HelloEngine/ComponentUIButton.h
+++ Source/HelloEngine/ComponentUIButton.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "ComponentUI.h"
 
+enum class ButtonState {
+	NORMAL,
+	ONPRESS,
+	//PRESSED,
+	ACTIVE
+};
+
 class ComponentUIButton : public ComponentUI
 {
+	public:
+
 	ComponentUIButton(GameObject* gameObject);
 	~ComponentUIButton();
 
 	void InputUpdate() override;
-
+	ButtonState State;
 private:
 	bool isFocused = false;
 };
