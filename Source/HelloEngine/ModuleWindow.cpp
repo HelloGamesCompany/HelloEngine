3da7b50124496ea373d3ab29ca134234b806153e
Adria Sellares <adsesa93@gmail.com>
2023-03-07 18:45:50 +0100

    Add Fullscreen in Runtime

--- Source/HelloEngine/ModuleWindow.cpp
+++ Source/HelloEngine/ModuleWindow.cpp
@@ -92,6 +92,11 @@ bool ModuleWindow::Init()
 		}
 	}
 
+#ifndef STANDALONE
+	S_SetTitle("Project Wastelanders - Vertical Slice 1");
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+#endif // !STANDALONE
+
 	S_SetTitle("Hello Engine");
 
 	return ret;
