af3388238d693d5456f1c0f1c2a415e14b20e0b2
Xavierlm11 <xaviloma11@gmail.com>
2023-03-08 21:14:18 +0100

    smooth Player movement keyboard

--- Output/Assets/Scripts/PlayerKeyboardMovement.h
+++ Output/Assets/Scripts/PlayerKeyboardMovement.h
@@ -13,12 +13,31 @@ public:
 
 	void MouseAim();
 
+	void Dash(float dt);
+
 	//player velocity on keyboard
 	float velk = 1.0f;
 
-	float maxVel = 1.0f;
-	float accel = 0.1f;
+	float maxVel = 0.15f;
+	float accel = 0.15f;
+	float brake = 0.5f;
+	bool moving = false;
+	bool braking = false;
+	bool movF = false;
+	bool movB = false;
+	bool movR = false;
+	bool movL = false;
+
+	float startDash = 0.0f;
+	float endDash = 0.05f;
+	float dashCooldown = 5.0f;
+	bool dashActive = false;
+	bool dashEnable = true;
+
 	API_Transform cam;
+	API_GameObject finalCam;
+	API_Transform MainCam;
+	API_Transform StarShipCam;
 
 private:
 	float _angle;
