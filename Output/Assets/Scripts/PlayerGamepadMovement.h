00c2b4e0176e9e1397dfff9b4dabe362c3738904
Xavierlm11 <xaviloma11@gmail.com>
2023-03-08 15:04:42 +0100

    provisional Smooth player movement (gamepad)

--- Output/Assets/Scripts/PlayerGamepadMovement.h
+++ Output/Assets/Scripts/PlayerGamepadMovement.h
@@ -17,7 +17,15 @@ public:
 	float vel = 1.0f;
 
 	float maxVel = 0.5f;
-	float accel = 0.06f;
+	float accel = 0.05f;
+	float brake = 0.05f;
+	bool moving = false;
+	bool braking = false;
+	bool movF = false;
+	bool movB = false;
+	bool movR = false;
+	bool movL = false;
+
 
 	API_Transform cam;
 
