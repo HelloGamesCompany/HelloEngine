d5151557af23c6703eccaa270f4262713f651ace
Ruboki <ruboki.reuchi@gmail.com>
2023-03-11 15:54:56 +0100

    Added UsingGampad funtion to Input API

--- Output/HelloAPI/EngineSource/API/API_Input.cpp
+++ Output/HelloAPI/EngineSource/API/API_Input.cpp
@@ -5,63 +5,70 @@
 
 KeyState API::Input::GetKey(KeyCode key)
 {
-	if (!LayerGame::detectInput)
-		return KeyState::KEY_IDLE;
-	return (KeyState)ModuleInput::S_GetKey((int)key);
+    if (!LayerGame::detectInput)
+        return KeyState::KEY_IDLE;
+    return (KeyState)ModuleInput::S_GetKey((int)key);
 }
 
 KeyState API::Input::GetMouseButton(MouseButton mouseButton)
 {
-	if (!LayerGame::detectInput)
-		return KeyState::KEY_IDLE;
-	return (KeyState)ModuleInput::S_GetMouseButton((int)mouseButton);
+    if (!LayerGame::detectInput)
+        return KeyState::KEY_IDLE;
+    return (KeyState)ModuleInput::S_GetMouseButton((int)mouseButton);
 }
 
 TO_API KeyState API::Input::GetGamePadButton(GamePadButton gamePadButton)
 {
-	if (!LayerGame::detectInput)
-		return KeyState::KEY_IDLE;
-	return (KeyState)ModuleInput::S_GetGamePadButton((GamePad)gamePadButton);
+    if (!LayerGame::detectInput)
+        return KeyState::KEY_IDLE;
+    return (KeyState)ModuleInput::S_GetGamePadButton((GamePad)gamePadButton);
 }
 
 TO_API int API::Input::GetGamePadAxis(GamePadAxis gamePadAxis)
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetGamePadAxis((SDL_GameControllerAxis)gamePadAxis);
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetGamePadAxis((SDL_GameControllerAxis)gamePadAxis);
 }
 
 int API::Input::GetMouseX()
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetMouseX();
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetMouseX();
 }
 
 int API::Input::GetMouseY()
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetMouseY();
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetMouseY();
 }
 
 int API::Input::GetMouseZ()
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetMouseZ();
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetMouseZ();
 }
 
 int API::Input::GetMouseXMotion()
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetMouseXMotion();
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetMouseXMotion();
 }
 
 int API::Input::GetMouseYMotion()
 {
-	if (!LayerGame::detectInput)
-		return 0;
-	return ModuleInput::S_GetMouseYMotion();
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_GetMouseYMotion();
+}
+
+bool API::Input::UsingGamepad()
+{
+    if (!LayerGame::detectInput)
+        return 0;
+    return ModuleInput::S_IsUsingController();
 }
