01570a3a8ed9fcd171115fb5cf855114e52d02bb
Biel Rabasa <bielrabasa@gmail.com>
2023-03-13 19:47:30 +0100

    PlayerKeyboard Basic Movement

--- Output/Assets/Scripts/Player/PlayerMove.cpp
+++ Output/Assets/Scripts/Player/PlayerMove.cpp
@@ -5,24 +5,22 @@ HELLO_ENGINE_API_C PlayerMove* CreatePlayerMove(ScriptToInspectorInterface* scri
     PlayerMove* classInstance = new PlayerMove();
     //Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
     script->AddDragFloat("Velocity", &classInstance->vel);
-    script->AddDragFloat("Max Velocity", &classInstance->maxVel);
+    script->AddDragFloat("CurrentVelocity", &classInstance->currentInput);
+   /* script->AddDragFloat("Max Velocity", &classInstance->maxVel);
     script->AddDragFloat("Acceleration", &classInstance->accel);
-    script->AddDragFloat("Brake", &classInstance->brake);
-    script->AddDragBoxGameObject("Aux Cam", &classInstance->finalCam);
+    script->AddDragFloat("Brake", &classInstance->brake);*/
+    /*script->AddDragBoxGameObject("Aux Cam", &classInstance->finalCam);
     script->AddDragBoxTransform("Camera player", &classInstance->MainCam);
     script->AddDragBoxTransform("Camera Starship", &classInstance->StarShipCam);
     script->AddDragBoxAnimationPlayer("Animation player", &classInstance->animationPlayer);
     script->AddDragBoxAnimationResource("Idle", &classInstance->idleAnim);
-    script->AddDragBoxAnimationResource("Walk", &classInstance->walkAnim);
+    script->AddDragBoxAnimationResource("Walk", &classInstance->walkAnim);*/
     return classInstance;
 }
 
 void PlayerMove::Start()
 {
-    aimAngle = 0.0f;
-    vel = 0.0f;
-    playingAnim = idleAnim;
-    animationPlayer.Play();
+    transform = gameObject.GetTransform();
 }
 
 void PlayerMove::Update()
@@ -30,29 +28,13 @@ void PlayerMove::Update()
     usingGamepad = Input::UsingGamepad();
     dt = Time::GetDeltaTime();
 
-    //SwapCamera();
-
     Aim();
 
-}
+    API_Vector2 input = GetMoveInput();
+    currentInput = input.Distance(API_Vector2::S_Zero());
 
-void PlayerMove::SwapCamera()
-{
-    SwapCam* cameraScript = (SwapCam*)finalCam.GetScript("SwapCam");
-
-    if (cameraScript != nullptr) {
-        if (cameraScript->mainCamActive) {
-            cam = MainCam;
-        }
-        else {
-            cam = StarShipCam;
-        }
-    }
-}
-
-void PlayerMove::Movement()
-{
-
+    input *= vel * dt;
+    transform.Translate(input.x, 0, input.y);
 }
 
 void PlayerMove::Aim()
@@ -76,4 +58,34 @@ void PlayerMove::Aim()
     {
 
     }
+}
+
+API_Vector2 PlayerMove::GetMoveInput()
+{
+    API_Vector2 input;
+
+    //Gamepad
+    if (usingGamepad) {
+        input.x = Input::GetGamePadAxis(GamePadAxis::AXIS_RIGHTX);
+        input.y = Input::GetGamePadAxis(GamePadAxis::AXIS_RIGHTY);
+
+        if (abs(input.x) < 10000 && abs(input.y) < 10000) return API_Vector2::S_Zero();
+
+        return input / 32000.0f;
+    }
+
+    //Keyboard
+    input.x = Input::GetKey(KeyCode::KEY_A) == KeyState::KEY_REPEAT;
+    input.x -= Input::GetKey(KeyCode::KEY_D) == KeyState::KEY_REPEAT;
+    input.y = Input::GetKey(KeyCode::KEY_W) == KeyState::KEY_REPEAT;
+    input.y -= Input::GetKey(KeyCode::KEY_S) == KeyState::KEY_REPEAT;
+
+    //Diagonal movement
+    if (input.x != 0.0f && input.y != 0.0f) {
+        // sqrt(2)/2 = 0.71
+        input.x *= 0.71f;
+        input.y *= 0.71f;
+    }
+
+    return input;
 }
--- Output/Assets/Scripts/Player/PlayerMove.h
+++ Output/Assets/Scripts/Player/PlayerMove.h
@@ -11,35 +11,18 @@ public:
     void Start() override;
     void Update() override;
 
-    void SwapCamera();
-    void Movement();
     void Aim();
+    API_Vector2 GetMoveInput();
 
     bool usingGamepad;
     float dt;
 
-    float vel = 0.0f;
-    float maxVel = 0.15f;
-    float accel = 0.15f;
-    float brake = 0.4f;
-    bool moving = false;
-    bool braking = false;
-    bool movF = false;
-    bool movB = false;
-    bool movR = false;
-    bool movL = false;
+    //Movement
+    float vel = 1.0f;
+    float currentInput = 0.0f; //test
+    API_Transform transform;
 
-    API_Transform cam;
-    API_GameObject finalCam;
-    API_Transform MainCam;
-    API_Transform StarShipCam;
-
-    API_AnimationPlayer animationPlayer;
-    uint idleAnim = 0;
-    uint walkAnim = 0;
-    uint currentAnim = 0;
-    uint playingAnim = 0;
-
+    //Shooting
     float aimAngle;
 };
 
