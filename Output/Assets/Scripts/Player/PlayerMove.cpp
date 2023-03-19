d7facea69f4762e6bdd6244480aece80427cba71
Biel Rabasa <bielrabasa@gmail.com>
2023-03-19 21:57:22 +0100

    PlayerDash & Input

--- Output/Assets/Scripts/Player/PlayerMove.cpp
+++ Output/Assets/Scripts/Player/PlayerMove.cpp
@@ -5,10 +5,12 @@ HELLO_ENGINE_API_C PlayerMove* CreatePlayerMove(ScriptToInspectorInterface* scri
     PlayerMove* classInstance = new PlayerMove();
     //Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
     script->AddDragFloat("Velocity", &classInstance->vel);
-    script->AddDragFloat("Current Velocity", &classInstance->currentVel);
+    //script->AddDragFloat("Current Velocity", &classInstance->currentVel);
     script->AddDragFloat("SecToMaxVel", &classInstance->secToMaxVel);
     script->AddDragFloat("SecToZeroVel", &classInstance->secToZeroVel);
-    script->AddDragFloat("Current Input", &classInstance->currentInput);
+    //script->AddDragFloat("Current Input", &classInstance->currentInput);
+    script->AddDragFloat("Dash Time", &classInstance->dashTime);
+    script->AddDragFloat("Dash Distance", &classInstance->dashDistance);
 
     return classInstance;
 }
@@ -24,6 +26,26 @@ void PlayerMove::Update()
     usingGamepad = Input::UsingGamepad();
     dt = Time::GetDeltaTime();
     Aim();
+
+    if (dashAvailable && !isDashing && DashInput())
+    {
+        isDashing = true;
+        //dashAvailable = false; TODO: UNCOMMENT
+        dashDepartTime = 0.0f;
+        dashInitialPos = transform.GetLocalPosition();
+        float norm = sqrt(pow(lastMovInput.x, 2) + pow(lastMovInput.y, 2));
+        API_Vector3 movDir;
+        movDir.x = lastMovInput.x / norm;
+        movDir.y = 0.0f;
+        movDir.z = lastMovInput.y / norm;
+        dashFinalPos = transform.GetLocalPosition() + movDir * dashDistance; //transform.GetForward() // for looking dir
+    }
+
+    if (isDashing)
+    {
+        Dash();
+        return; //NO MORE MOVEMENT
+    }
 
     API_Vector2 input = GetMoveInput();
     currentInput = input.Distance(API_Vector2::S_Zero());   //TEST
@@ -66,8 +88,36 @@ float PlayerMove:: Lerp(float a, float b, float time)
     return a + time * (b - a);
 }
 
+void PlayerMove::Dash()
+{
+    API_Vector3 newPos;
+    newPos.x = Lerp(dashInitialPos.x, dashFinalPos.x, dashDepartTime / dashTime);
+    newPos.y = Lerp(dashInitialPos.y, dashFinalPos.y, dashDepartTime / dashTime);
+    newPos.z = Lerp(dashInitialPos.z, dashFinalPos.z, dashDepartTime / dashTime);
+    transform.SetPosition(newPos);
+
+    if (dashDepartTime >= dashTime)
+    {
+        isDashing = false;
+        return;
+    }
+
+    dashDepartTime += dt;
+    if (dashDepartTime > dashTime) dashDepartTime = dashTime;
+}
+
+bool PlayerMove::DashInput()
+{
+    if (usingGamepad)
+        return Input::GetGamePadButton(GamePadButton::BUTTON_LEFT_SHOULDER) == KeyState::KEY_DOWN;
+
+    return Input::GetKey(KeyCode::KEY_SPACE) == KeyState::KEY_DOWN;
+}
+
 void PlayerMove::Aim()
 {
+    API_Vector2 normalizedInput;
+    
     if (usingGamepad)
     {
         API_Vector2 input;
@@ -76,17 +126,32 @@ void PlayerMove::Aim()
 
         if (abs(input.x) < 10000 && abs(input.y) < 10000) return;
 
-        API_Vector2 normalizedInput;
-        normalizedInput.x = input.x / sqrt(pow(input.x, 2) + pow(input.y, 2));
-        normalizedInput.y = input.y / sqrt(pow(input.x, 2) + pow(input.y, 2));
-
-        aimAngle = atan2(normalizedInput.y, normalizedInput.x) * RADTODEG - 90.0f;
-        gameObject.GetTransform().SetRotation(0, aimAngle, 0);
+        float norm = sqrt(pow(input.x, 2) + pow(input.y, 2));
+        normalizedInput.x = input.x / norm;
+        normalizedInput.y = input.y / norm;
     }
     else
     {
+        normalizedInput.x = Input::GetKey(KeyCode::KEY_LEFT) == KeyState::KEY_REPEAT;
+        normalizedInput.x -= Input::GetKey(KeyCode::KEY_RIGHT) == KeyState::KEY_REPEAT;
+        normalizedInput.y = Input::GetKey(KeyCode::KEY_UP) == KeyState::KEY_REPEAT;
+        normalizedInput.y -= Input::GetKey(KeyCode::KEY_DOWN) == KeyState::KEY_REPEAT;
 
+        if (normalizedInput.x != 0.0f && normalizedInput.y != 0.0f) {
+            // sqrt(2)/2 = 0.71
+            normalizedInput.x *= 0.71f;
+            normalizedInput.y *= 0.71f;
+        }
     }
+
+    //Get last input to keep Aim angle
+    if (normalizedInput.x == 0.0f && normalizedInput.y == 0.0f)
+        normalizedInput = lastAimInput;
+    else
+        lastAimInput = normalizedInput;
+
+    aimAngle = atan2(normalizedInput.y, normalizedInput.x) * RADTODEG - 90.0f;
+    gameObject.GetTransform().SetRotation(0, aimAngle, 0);
 }
 
 API_Vector2 PlayerMove::GetMoveInput()
@@ -96,9 +161,12 @@ API_Vector2 PlayerMove::GetMoveInput()
     //Gamepad
     if (usingGamepad) {
         input.x = Input::GetGamePadAxis(GamePadAxis::AXIS_LEFTX);
-        input.y = -Input::GetGamePadAxis(GamePadAxis::AXIS_LEFTY);
+        input.y = Input::GetGamePadAxis(GamePadAxis::AXIS_LEFTY);
 
         if (abs(input.x) < 10000 && abs(input.y) < 10000) return API_Vector2::S_Zero();
+
+        if (input.x > 32000.0f) input.x = 32000.0f;
+        if (input.y > 32000.0f) input.y = 32000.0f;
 
         return input / 32000.0f;
     }
--- Output/Assets/Scripts/Player/PlayerMove.h
+++ Output/Assets/Scripts/Player/PlayerMove.h
@@ -14,12 +14,15 @@ public:
     void Aim();
     API_Vector2 GetMoveInput();
     float Lerp(float a, float b, float time);
+    void Dash();
+    bool DashInput();
 
     bool usingGamepad;
     float dt;
 
     //Movement
     API_Vector2 lastMovInput = { 0.0f, 0.0f };
+    API_Vector2 lastAimInput = { 0.0f, 0.0f };
     float vel = 1.0f;
     float currentVel = 0.0f;
     float currentInput = 0.0f; //test
@@ -27,6 +30,15 @@ public:
     float secToMaxVel = 0.0f;
     float secToZeroVel = 0.0f;
     API_Transform transform;
+
+    //Dash
+    bool isDashing = false;
+    bool dashAvailable = true;
+    float dashDepartTime = 0.0f;
+    API_Vector3 dashInitialPos;
+    API_Vector3 dashFinalPos;
+    float dashTime = 0.5f;
+    float dashDistance = 5.0f;
 
     //Shooting
     float aimAngle;
