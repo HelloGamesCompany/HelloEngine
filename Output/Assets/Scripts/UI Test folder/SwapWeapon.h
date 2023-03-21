fdbd9704e598dcc9dbbc3a01c9a7c84a892ec13f
Pol Maresch <polmaresch@gmail.com>
2023-03-21 21:13:50 +0100

    Swap Weapon and dash finished

--- Output/Assets/Scripts/UI Test folder/SwapWeapon.h
+++ Output/Assets/Scripts/UI Test folder/SwapWeapon.h
@@ -11,7 +11,34 @@ public:
 	void Start() override; 
 	void Update() override;
 
-	uint Texture_Weapon;
-	API_Material Material_Weapon;
+	void SwapWeapon1();
+	void SwapWeapon2();
+	void SwapWeapon3();
+	void Dash();
+
+	//Weapon 1  
+	uint Texture_Weapon_1_on;
+	uint Texture_Weapon_1_off;
+	API_Material Material_Weapon_1;
+	
+	//Weapon 2 
+	uint Texture_Weapon_2_on;
+	uint Texture_Weapon_2_off;
+	API_Material Material_Weapon_2;
+	
+	//Weapon 3  
+	uint Texture_Weapon_3_on;
+	uint Texture_Weapon_3_off;
+	API_Material Material_Weapon_3;
+
+	API_GameObject Active_Bar_1;
+	API_GameObject Active_Bar_2;
+	API_GameObject Active_Bar_3;
+
+	//Dash
+	uint Texture_Dash_on;
+	uint Texture_Dash_off;
+	API_Material Material_Dash;
+	bool activeDash = true;
 };
 
