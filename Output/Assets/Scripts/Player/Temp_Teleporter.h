96e22c6b00f0dacf8a9d149e478bcc000f8c0681
Adria Sellares <adsesa93@gmail.com>
2023-03-23 06:17:32 +0100

    Add Teleporter

---
+++ Output/Assets/Scripts/Player/Temp_Teleporter.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "API/HelloBehavior.h"
+#include "ScriptToInspectorInterface.h"
+#include "Macro.h"
+
+#include "API/API.h"
+
+class Temp_Teleporter : HelloBehavior
+{
+public:
+	void Start() override; 
+	void Update() override;
+
+	void OnCollisionEnter(API_RigidBody other);
+
+	API_Transform point1;
+	API_Transform point2;
+
+	bool isPoint1 = true;
+
+
+};
+
