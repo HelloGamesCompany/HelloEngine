a1c172cbf6df2d5aa28ccd12d01225afd09da69f
MaralGS <algarselma@gmail.com>
2023-03-29 20:38:25 +0200

    Created Script For the Ability tree

---
+++ Output/Assets/Scripts/AbilityTreeScript.cpp
@@ -0,0 +1,51 @@
+#include "AbilityTreeScript.h"
+//MADE BY ALEX GARCIA SELMA
+HELLO_ENGINE_API_C AbilityTreeScript* CreateAbilityTreeSScript(ScriptToInspectorInterface* script)
+{
+	AbilityTreeScript* classInstance = new AbilityTreeScript();
+	//Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
+	script->AddDragBoxUIButton("Movement", &classInstance->AbilityList1);
+	script->AddDragBoxUIButton("AbilityList2", &classInstance->AbilityList2);
+	script->AddDragBoxUIButton("AbilityList3", &classInstance->AbilityList3);
+	script->AddDragBoxUIButton("AbilityList4", &classInstance->AbilityList4);
+
+	//panels
+	script->AddDragBoxGameObject("PanelMovement", &classInstance->PanelAbility1);
+	script->AddDragBoxGameObject("PanelAbility2", &classInstance->PanelAbility2);
+	script->AddDragBoxGameObject("PanelAbility3", &classInstance->PanelAbility3);
+	script->AddDragBoxGameObject("PanelAbility4", &classInstance->PanelAbility4);
+	return classInstance;
+}
+
+void AbilityTreeScript::Start()
+{
+
+}
+void AbilityTreeScript::Update()
+{
+	if (AbilityList1.OnHovered())
+	{
+		PanelAbility1.SetActive(true);
+		PanelAbility2.SetActive(false);
+	}
+
+	if (AbilityList2.OnHovered())
+	{
+		PanelAbility1.SetActive(false);
+		PanelAbility2.SetActive(true);
+		PanelAbility3.SetActive(false);
+	}
+
+	if (AbilityList3.OnHovered())
+	{
+		PanelAbility1.SetActive(false);
+		PanelAbility2.SetActive(true);
+		PanelAbility3.SetActive(false);
+	}
+
+	if (AbilityList4.OnHovered())
+	{
+		PanelAbility3.SetActive(false);
+		PanelAbility4.SetActive(true);
+	}
+}
