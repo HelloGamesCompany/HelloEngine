f74937501b13db77daf8b53b26d6906197777c62
Ruboki <ruboki.reuchi@gmail.com>
2023-04-06 16:55:00 +0200

    player indicator on map (not finished)

---
+++ Output/Assets/Scripts/UI Test folder/PlayerIndicator.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "API/HelloBehavior.h"
+#include "ScriptToInspectorInterface.h"
+#include "Macro.h"
+
+#include "API/API.h"
+
+class PlayerIndicator : HelloBehavior
+{
+public:
+    void Start() override;
+    void Update() override;
+
+    API_GameObject player;
+    API_Vector2 originIndicatorPos;
+    API_Vector2 originPlayerPos;
+
+    float movementReductionX = 1000.0f;
+    float movementReductionY = 1000.0f;
+};
+
