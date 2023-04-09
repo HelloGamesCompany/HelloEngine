82af0a2a92d36fc1ec08d605c8e595bc798645ad
Ruboki <ruboki.reuchi@gmail.com>
2023-04-09 16:51:39 +0200

    armory load and save

--- Output/Assets/Scripts/ArmoryUpgratteButtons.cpp
+++ Output/Assets/Scripts/ArmoryUpgratteButtons.cpp
@@ -1,86 +1,123 @@
 #include "ArmoryUpgratteButtons.h"
 HELLO_ENGINE_API_C ArmoryUpgratteButtons* CreateArmoryUpgratteButtons(ScriptToInspectorInterface* script)
 {
-	ArmoryUpgratteButtons* classInstance = new ArmoryUpgratteButtons();
+    ArmoryUpgratteButtons* classInstance = new ArmoryUpgratteButtons();
 
-	script->AddDragBoxUIButton("Upgrate 1", &classInstance->Upgrate1);
-	script->AddDragBoxUIButton("Upgrate 2", &classInstance->Upgrate2);
-	script->AddDragBoxUIButton("Upgrate 3", &classInstance->Upgrate3);
+    script->AddDragBoxUIButton("Upgrate 1", &classInstance->Upgrate1);
+    script->AddDragBoxUIButton("Upgrate 2", &classInstance->Upgrate2);
+    script->AddDragBoxUIButton("Upgrate 3", &classInstance->Upgrate3);
 
-	script->AddDragBoxUIInput("Panel Upgrate", &classInstance->PanelUpgrate);
+    script->AddDragBoxUIInput("Panel Upgrate", &classInstance->PanelUpgrate);
 
-	script->AddDragBoxGameObject("Weapon Associated", &classInstance->currentWeapon);
-	script->AddDragBoxGameObject("Player", &classInstance->Player);
+    script->AddDragBoxGameObject("Weapon Associated", &classInstance->currentWeapon);
+    script->AddDragBoxGameObject("Player", &classInstance->Player);
 
-	script->AddDragBoxUIInput("List Weapons", &classInstance->SelectWeaponList);
+    script->AddDragBoxUIInput("List Weapons", &classInstance->SelectWeaponList);
 
-	script->AddDragBoxUIText("Text Blueprints", &classInstance->bluePrintText);
+    script->AddDragBoxUIText("Text Blueprints", &classInstance->bluePrintText);
 
-	return classInstance;
+    script->AddDragInt("Gun Index", &classInstance->gunIndex);
+
+    return classInstance;
 }
 
 void ArmoryUpgratteButtons::Start()
 {
-	/*weaponInstance = (ArmoryWeaponSelect*)currentWeapon.GetScript("ArmoryWeaponSelect");
-	if (weaponInstance == nullptr)
-	{
-		Console::Log("Armor Weapon Select is null pointer in the Associated weapon");
-	}*/
-	_playerStorage = (PlayerStorage*)Player.GetScript("PlayerStorage");
-	if (_playerStorage == nullptr)
-	{
-		Console::Log("Player is null pointer");
-	}
-	upgrade1 = false;
-	upgrade2 = false;
+    switch (gunIndex)
+    {
+    case 1:
+        gunLevel = API_QuickSave::GetBool("semiautomatic_level");
+        break;
+    case 2:
+        gunLevel = API_QuickSave::GetBool("automatic_level");
+        break;
+    case 3:
+        gunLevel = API_QuickSave::GetBool("burst_level");
+        break;
+    case 4:
+        gunLevel = API_QuickSave::GetBool("shotgun_level");
+        break;
+    case 5:
+        gunLevel = API_QuickSave::GetBool("handgun_level");
+        break;
+    default:
+        break;
+    }
 
-	//bluePrintText.GetGameObject().GetMeshRenderer()
+    _playerStorage = (PlayerStorage*)Player.GetScript("PlayerStorage");
+    if (_playerStorage == nullptr) Console::Log("Player Storage missing in ArmaryUpgradeButtons.");
+
+    if (gunLevel > 0) Upgrate1.SetBlocked(true);
+    if (gunLevel > 1) Upgrate2.SetBlocked(true);
+    if (gunLevel > 2) Upgrate3.SetBlocked(true);
+
+    //bluePrintText.GetGameObject().GetMeshRenderer()
 }
 void ArmoryUpgratteButtons::Update()
 {
-	if (Input::GetGamePadButton(GamePadButton::BUTTON_B) == KeyState::KEY_DOWN)
-	{
-		SelectWeaponList.SetEnable(true);
-		PanelUpgrate.SetEnable(false);
-	}
+    if (Input::GetGamePadButton(GamePadButton::BUTTON_B) == KeyState::KEY_DOWN)
+    {
+        SelectWeaponList.SetEnable(true);
+        PanelUpgrate.SetEnable(false);
+    }
 
-	if (Upgrate1.OnPress())
-	{
-		Upgrade1();
-		Upgrate2.SetBlocked(false);
-		Console::Log("Pepep");
-		if (_playerStorage != nullptr)
-		{
-			bluePrintText.SetText(std::to_string(_playerStorage->upgradeBlueprintAmount).c_str());
-		}
-		upgrade1 = true;
-	}
+    if (!_playerStorage) return;
 
-	if (Upgrate2.OnPress() && upgrade1)
-	{
-		Upgrate3.SetBlocked(false);
-		Upgrade2();
-		upgrade2 = true;
-	}
+    if (Upgrate1.OnPress() && _playerStorage->upgradeBlueprintAmount > 0)
+    {
+        UpgradeGun();
+        Upgrate2.SetBlocked(false);
+        if (_playerStorage != nullptr)
+        {
+            bluePrintText.SetText(std::to_string(_playerStorage->upgradeBlueprintAmount).c_str());
+        }
+    }
 
-	if (Upgrate3.OnPress() && upgrade2)
-	{
-		Upgrade3();
-	}
+    if (Upgrate2.OnPress() && _playerStorage->upgradeBlueprintAmount > 0)
+    {
+        UpgradeGun();
+        Upgrate3.SetBlocked(false);
+        if (_playerStorage != nullptr)
+        {
+            bluePrintText.SetText(std::to_string(_playerStorage->upgradeBlueprintAmount).c_str());
+        }
+    }
+
+    if (Upgrate3.OnPress() && _playerStorage->upgradeBlueprintAmount > 0)
+    {
+        UpgradeGun();
+        if (_playerStorage != nullptr)
+        {
+            bluePrintText.SetText(std::to_string(_playerStorage->upgradeBlueprintAmount).c_str());
+        }
+    }
 }
 
-void ArmoryUpgratteButtons::Upgrade1()
+void ArmoryUpgratteButtons::UpgradeGun()
 {
-	Console::Log("1");
-}
+    _playerStorage->upgradeBlueprintAmount--;
+    _playerStorage->SaveData();
 
-void ArmoryUpgratteButtons::Upgrade2()
-{
-	Console::Log("2");
+    gunLevel++;
 
-}
-
-void ArmoryUpgratteButtons::Upgrade3()
-{
-	Console::Log("3");
+    switch (gunIndex)
+    {
+    case 1:
+        API_QuickSave::SetBool("semiautomatic_level", gunLevel);
+        break;
+    case 2:
+        API_QuickSave::SetBool("automatic_level", gunLevel);
+        break;
+    case 3:
+        API_QuickSave::SetBool("burst_level", gunLevel);
+        break;
+    case 4:
+        API_QuickSave::SetBool("shotgun_level", gunLevel);
+        break;
+    case 5:
+        API_QuickSave::SetBool("handgun_level", gunLevel);
+        break;
+    default:
+        break;
+    }
 }
