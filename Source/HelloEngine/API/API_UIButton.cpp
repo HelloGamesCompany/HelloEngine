bb9757162828199046d0621fcb95a5b97e0f1275
Adria Sellares <adsesa93@gmail.com>
2023-04-09 21:30:19 +0200

    Add OnRelease in API_Button

--- Source/HelloEngine/API/API_UIButton.cpp
+++ Source/HelloEngine/API/API_UIButton.cpp
@@ -56,9 +56,6 @@ bool API::API_UIButton::OnHovered()
 	return _UIButton->State == ButtonState::HOVERED;
 }
 
-
-
-
 ComponentUIButton* API::API_UIButton::GetComponent()
 {
 	return _UIButton;
@@ -72,11 +69,31 @@ void API::API_UIButton::SetComponent(ComponentUIButton* component)
 
 bool API::API_UIButton::getIsBlocked()
 {
+	if (!_UIButton)
+	{
+		Engine::Console::S_Log("Trying to acces a NULLPTR UI Button");
+		return false;
+	}
 	 return _UIButton->isBlocked;
 }
 
 void API::API_UIButton::SetBlocked(bool set)
 {
+	if (!_UIButton)
+	{
+		Engine::Console::S_Log("Trying to acces a NULLPTR UI Button");
+		return;
+	}
 	_UIButton->isBlocked = set;
+}
+
+bool API::API_UIButton::OnRelease()
+{
+	if (!_UIButton)
+	{
+		Engine::Console::S_Log("Trying to acces a NULLPTR UI Button");
+		return false;
+	}
+	return _UIButton->isReleased;
 }
 
