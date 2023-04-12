d527daf6d95505200ec5a503adec3a04a848d748
Pol Maresch <polmaresch@gmail.com>
2023-04-12 09:46:45 +0200

    Scripts Bars HUD Functional

--- Output/Assets/Scripts/UI Test folder/HpBar.cpp
+++ Output/Assets/Scripts/UI Test folder/HpBar.cpp
@@ -4,6 +4,9 @@ HELLO_ENGINE_API_C HpBar* CreateHpBar(ScriptToInspectorInterface* script)
     HpBar* classInstance = new HpBar();
     //Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
     script->AddDragBoxGameObject("Player Stats GO", &classInstance->playerStatsGO);
+    script->AddDragBoxUIImage("HP_BAR", &classInstance->hp_Bar);
+    script->AddDragBoxUIImage("HP_REGEN_BAR", &classInstance->hp__Regen_Bar);
+    script->AddDragBoxUIImage("SHIELD_BAR", &classInstance->shield_Bar);
     return classInstance;
 }
 
@@ -12,26 +15,45 @@ void HpBar::Start()
     playerStats = (PlayerStats*)playerStatsGO.GetScript("PlayerStats");
     if (playerStats == nullptr) Console::Log("Missing PlayerStats on HpBar Script.");
 
-    FirstPosX = gameObject.GetTransform().GetLocalPosition().x;
-    FirstPosY = gameObject.GetTransform().GetLocalPosition().y;
-    FirstPosZ = gameObject.GetTransform().GetLocalPosition().z;
+    hp_Bar.FillImage(0.5);
+    hp__Regen_Bar.GetGameObject().SetActive(false);
+    shield_Bar.FillImage(0);
+
 }
 void HpBar::Update()
 {
-    float finalScale;
-    if (playerStats) finalScale = playerStats->currentHp * 0.4f / playerStats->currentMaxHp;
-    else finalScale = 100.0f * 0.4f / 100.0f;
 
-    if (finalScale != gameObject.GetTransform().GetLocalScale().x) {
+}
 
-        float differenceHp = playerStats->currentMaxHp - playerStats->currentHp;
+void HpBar::HpPlayerbar(float vida_Player_value, float Regen_Player_value, bool Regen_Bar_Active)
+{
 
-        float offsetX = differenceHp * 0.0036f;
-        //float offsetY = differenceHp * 0.0014f;
-
-        //gameObject.GetTransform().SetPosition(FirstPosX - offsetX, FirstPosY - offsetY, gameObject.GetTransform().GetLocalPosition().z);
-        gameObject.GetTransform().SetPosition(FirstPosX - offsetX, FirstPosY, gameObject.GetTransform().GetLocalPosition().z);
+    if (Regen_Bar_Active == false)
+    {
+        hp__Regen_Bar.GetGameObject().SetActive(false);
+        hp_Bar.GetGameObject().SetActive(true);
+        Regen_Player_value = vida_Player_value;
+        hp_Bar.FillImage(vida_Player_value);
     }
+    
+    if (Regen_Bar_Active == true)
+    {
+        hp__Regen_Bar.GetGameObject().SetActive(true);
+        hp_Bar.GetGameObject().SetActive(false);
+        vida_Player_value = Regen_Player_value;
+        hp__Regen_Bar.FillImage(Regen_Player_value);
+    }
+}
 
-    gameObject.GetTransform().SetScale(finalScale, 0.12f, 0.5f);
+void HpBar::ShieldPlayerbar(float Shield_Player_value, bool Shield_Active)
+{
+    if (Shield_Active == true)
+    {
+        shield_Bar.FillImage(Shield_Player_value);
+    }
+    
+    if (Shield_Active == false)
+    {
+        shield_Bar.FillImage(0);
+    }
 }
