9059137348fe792b94a06cf35a5c47a8aca5f83d
Ruboki <ruboki.reuchi@gmail.com>
2023-04-12 14:19:23 +0200

    setting hability screen

--- Output/Assets/Scripts/AbilitySelect.cpp
+++ Output/Assets/Scripts/AbilitySelect.cpp
@@ -3,8 +3,8 @@ HELLO_ENGINE_API_C AbilitySelect* CreateAbilitySelect(ScriptToInspectorInterface
 {
     AbilitySelect* classInstance = new AbilitySelect();
 
-    script->AddDragBoxUIInput("Panel Type", &classInstance->mainPanel);
-    script->AddDragBoxUIInput("Panel Ability", &classInstance->abilityPanel);
+    script->AddDragBoxUIInput("Main Panel", &classInstance->mainPanel);
+    script->AddDragBoxUIInput("Current Panel", &classInstance->abilityPanel);
     script->AddDragBoxUIButton("AbilityButton", &classInstance->abilitySelect);
     //Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
     return classInstance;
