012a394b3de579a169c05ac6e839a9ba5d65349f
Adria Sellares <adsesa93@gmail.com>
2023-04-19 03:53:42 +0200

    Fix on ReImport metas for Meshes

--- Source/HelloEngine/ModuleFiles.cpp
+++ Source/HelloEngine/ModuleFiles.cpp
@@ -1013,7 +1013,6 @@ void ModuleFiles::RegenerateMetasRecursive(std::string& path, std::vector<std::s
                         {
                             ResourceMesh* mesh = (ResourceMesh*)model->modelMeshes[i];
                             mesh->modelUID = GUID;
-                            oldResources.push_back(mesh->UID);
                         }
                     }
                     if (res->type == ResourceType::MATERIAL) // We need to update materials AFTER shaders, so we save them and do them all later.
