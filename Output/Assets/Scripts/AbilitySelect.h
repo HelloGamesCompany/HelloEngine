e25d2bdd9cd615fa71d7aa91ca615bfae55cea71
MaralGS <algarselma@gmail.com>
2023-04-27 00:50:26 +0200

    HUB Scene Ability Screen Bug Fixing

--- Output/Assets/Scripts/AbilitySelect.h
+++ Output/Assets/Scripts/AbilitySelect.h
@@ -16,6 +16,12 @@ public:
     API_UIInput mainPanel;
     API_UIInput abilityPanel;
 
+    API_GameObject description1;
+    API_GameObject description2;
+    API_GameObject description3;
+    API_GameObject description4;
+    API_GameObject description5;
+
     bool waitFrame = false;
 };
 
