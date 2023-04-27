708ae26b830fc9f83146c601a539f39a3ffe9161
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-04-27 13:04:54 +0200

    Bugs Solved On Scene Lvl2 TP & Smoke Sxtractors

--- Output/Assets/Scripts/Quests/FindAndRescueGroot.cpp
+++ Output/Assets/Scripts/Quests/FindAndRescueGroot.cpp
@@ -1,11 +1,13 @@
 #include "FindAndRescueGroot.h"
 #include "../Player/PlayerStorage.h"
+#include "../FadeToBlack.h"
 
 HELLO_ENGINE_API_C FindAndRescueGroot* CreateFindAndRescueGroot(ScriptToInspectorInterface* script)
 {
 	FindAndRescueGroot* classInstance = new FindAndRescueGroot();
 	//Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
 	script->AddDragBoxGameObject("Player Storage GO", &classInstance->playerStorageGO);
+	script->AddDragBoxGameObject("Black Image", &classInstance->fadeToBlackGO);
 	return classInstance;
 }
 
@@ -13,6 +15,9 @@ void FindAndRescueGroot::Start()
 {
     playerStorage = (PlayerStorage*)playerStorageGO.GetScript("PlayerStorage");
     if (playerStorage == nullptr) Console::Log("PlayerStorage missing in FindAndRescue Script.");
+
+	fadeToBlackRef = (FadeToBlack*)fadeToBlackGO.GetScript("FadeToBlack");
+	if (!fadeToBlackRef) Console::Log("fadeToBlackRef Missing in Shipinlevel.");
 }
 void FindAndRescueGroot::Update()
 {
@@ -24,16 +29,30 @@ void FindAndRescueGroot::OnCollisionStay(API_RigidBody other)
     std::string detectionName = other.GetGameObject().GetName();
     if (detectionName == "Player")
     {
-        if (Input::GetGamePadButton(GamePadButton::BUTTON_X) == KeyState::KEY_DOWN || Input::GetKey(KeyCode::KEY_E) == KeyState::KEY_DOWN)
-        {
-            if (playerStorage)
-            {
-                playerStorage->skillPoints += 5;
-                playerStorage->SaveData();
-            }
-            API_QuickSave::SetBool("level2_completed", true);
- 
-        }
+
+		if (Input::GetGamePadButton(GamePadButton::BUTTON_X) == KeyState::KEY_DOWN)
+		{
+			fadeToBlackRef->fadeToBlack = true;
+
+			if (playerStorage)
+			{
+				playerStorage->skillPoints += 5;
+				playerStorage->SaveData();
+			}
+			API_QuickSave::SetBool("level2_completed", true);
+
+		}
+		if (Input::GetGamePadButton(GamePadButton::BUTTON_X) == KeyState::KEY_REPEAT)
+		{
+			timerToTp += Time::GetDeltaTime();
+
+			if (timerToTp >= 1.2f)
+			{
+
+				Scene::LoadScene("SpaceshipHUB_Scene.HScene");
+				timerToTp = 0.0f;
+			}
+		}
 
     }
 }
